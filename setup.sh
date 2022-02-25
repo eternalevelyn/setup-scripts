79afb121dc9983ef70a26df8b66647fd1bc2130a
obiequack <81878131+obiequack@users.noreply.github.com>
2022-02-24 22:31:28 -0700

    edit setup.sh

--- setup.sh
+++ setup.sh
@@ -6,3 +6,8 @@ sudo freshclam
 sudo systemctl enable --now xrdp
 sudo systemctl enable --now cockpit.socket
 gio open http://127.0.0.1:9090/
+clear
+touch explanation.txt
+echo "Welcome to Manjaro Linux setup scripts.  This script installs yay and dependencies, xfreerdp/a graphical frontend for xfreerdp, known as krdc, WINE for Windows programs, Steam and Lutris for games, LibreOffice, an office suite, ClamAV and a gui interface for it (clamtk), Cockpit, and qTox, xrdp (an RDP server), Netvisix for packet visualization, LibreWolf for browsing, and eDEX-UI for terminal emulations. Run './inst.sh' to see this message again." > explanation.txt
+cat explanation.txt
+echo "less explanation.txt" > inst.sh
