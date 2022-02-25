0248767dc4aedc5ba94f82c5693aa6303c917ef4
obiequack <81878131+obiequack@users.noreply.github.com>
2022-02-25 16:39:19 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -8,7 +8,7 @@ sudo systemctl enable --now cockpit.socket
 gio open http://127.0.0.1:9090/
 clear
 touch explanation.txt
-echo "Welcome to Manjaro Linux setup scripts.  This script installs yay and dependencies, xfreerdp/a graphical frontend for xfreerdp, known as krdc, WINE for Windows programs, Steam and Lutris for games, LibreOffice, an office suite, ClamAV and a gui interface for it (clamtk), Cockpit, and qTox, xrdp (an RDP server), Netvisix for packet visualization, LibreWolf for browsing, and eDEX-UI for terminal emulations. Run './inst.sh' to see this message again." > explanation.txt
+echo "Welcome to Manjaro Linux setup scripts.  This script installs yay and dependencies, xfreerdp/a graphical frontend for xfreerdp, known as krdc, WINE for Windows programs, Steam and Lutris for games, LibreOffice, an office suite, ClamAV and a gui interface for it (clamtk), Cockpit, and qTox, xrdp (an RDP server), Netvisix for packet visualization, LibreWolf for browsing, and eDEX-UI for terminal emulations. Run 'sudo ./inst.sh' to see this message again." > explanation.txt
 cat explanation.txt
 echo "cat explanation.txt" > inst.sh
 sudo chmod a+rx inst.sh
